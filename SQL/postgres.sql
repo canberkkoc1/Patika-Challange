e0ede6772dd3b42e9af2a82baca33d64fe3a9a6e
canberkkoc1 <canberkkoc.94@gmail.com>
2022-01-16 11:18:41 +0300

    DISTINCT-COUNT

--- SQL/postgres.sql
+++ SQL/postgres.sql
@@ -54,3 +54,25 @@ SELECT title FROM film WHERE title ILIKE '%t%t%t%t'
 
 
 SELECT title FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
+
+
+-- film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+
+SELECT DISTINCT replacement_cost FROM film
+
+-- film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+
+SELECT  COUNT(DISTINCT replacement_cost) FROM film
+
+-- film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+
+SELECT COUNT(title) from film WHERE title LIKE 'T%' AND rating = 'G'
+
+-- country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+SELECT COUNT(country) from country WHERE country LIKE '_____'
+
+
+-- city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+SELECT city from city WHERE city ILIKE '%R'
